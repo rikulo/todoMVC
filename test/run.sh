1c310927fdc018519b324b98824ca7d29cb9871b
tomyeh <tomyeh@potix.com>
2013-06-04 17:47:53 +0800

    use dartanalyzer instead

--- test/run.sh
+++ test/run.sh
@@ -5,11 +5,11 @@ set -e
 
 DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 
-# Note: dart_analyzer needs to be run from the root directory for proper path
+# Note: dartanalyzer needs to be run from the root directory for proper path
 # canonicalization.
 pushd $DIR/..
 echo Analyzing web/app.dart
-dart_analyzer --fatal-warnings --fatal-type-errors web/app.dart \
+dartanalyzer --fatal-warnings --fatal-type-errors web/app.dart \
   || echo -e "Ignoring analyzer errors"
 
 rm -rf out/*
