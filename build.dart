e985c46d01709d9743cea1f6db3a114f9bf7df04
tomyeh <tomyeh@potix.com>
2013-10-29 19:22:06 +0800

    Remove the use of deprecated Options

--- build.dart
+++ build.dart
@@ -1,7 +1,6 @@
-import 'dart:io' show Options;
 import 'package:rikulo_uxl/uc.dart' show build;
 
-void main() {
+void main(List<String> arguments) {
   // invoke UXL auto-compile
-  build(new Options().arguments);
+  build(arguments);
 }
